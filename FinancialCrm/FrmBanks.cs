25371167465cf51aea602cdcd01551e41045a321
Kadir Can Karataş <kadircankaratass@gmail.com>
2025-07-30 13:59:46 +0300

    Add project files.

---
+++ FinancialCrm/FrmBanks.cs
@@ -0,0 +1,45 @@
+﻿using FinancialCrm.Models;
+using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FinancialCrm
+{
+    public partial class FrmBanks : Form
+    {
+        public FrmBanks()
+        {
+            InitializeComponent();
+        }
+        FinancialCrmDbEntities1 db = new FinancialCrmDbEntities1();
+        private void FrmBanks_Load(object sender, EventArgs e)
+        {
+            var ziraatBankBalance = db.Banks.Where(x => x.BankTitle == "Ziraat Bankası").Select(x => x.BankBalance).FirstOrDefault();
+            var vakifBankBalance = db.Banks.Where(x => x.BankTitle == "Vakıfbank").Select(x => x.BankBalance).FirstOrDefault();
+            var isBankBalance = db.Banks.Where(x => x.BankTitle == "İş Bankası").Select(x => x.BankBalance).FirstOrDefault();
+
+            lblZiraatBankBalance.Text = ziraatBankBalance.ToString() + " TL";
+            lblVakifBankBalance.Text = vakifBankBalance.ToString() + " TL";
+            lblİsBankBalance.Text = isBankBalance.ToString() + " TL";
+
+            //Banka Hareketleri
+
+            var bankProcess1 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(1).FirstOrDefault();
+            lblBankProcess1.Text = bankProcess1.Description + " " + bankProcess1.Amount + " TL" + " " + bankProcess1.ProcessDate;
+
+            var bankProcess2 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(2).Skip(1).FirstOrDefault();
+            lblBankProcess2.Text = bankProcess2.Description + " " + bankProcess2.Amount + " TL" + " " + bankProcess2.ProcessDate;
+
+            var bankProcess3 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(3).Skip(2).FirstOrDefault();
+            lblBankProcess3.Text = bankProcess3.Description + " " + bankProcess3.Amount + " TL" + " " + bankProcess3.ProcessDate;
+
+            var bankProcess4 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(4).Skip(3).FirstOrDefault();
+            lblBankProcess4.Text = bankProcess4.Description + " " + bankProcess4.Amount + " TL" + " " + bankProcess4.ProcessDate;
+
+            var bankProcess5 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(5).Skip(4).FirstOrDefault();
+            lblBankProcess5.Text = bankProcess5.Description + " " + bankProcess5.Amount + " TL" + " " + bankProcess5.ProcessDate;
+
+        }
+    }
+}
